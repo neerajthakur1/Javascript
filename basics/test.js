60dc90171beee54937b6bdb3d0f0e273efa8113f
neerajthakur1 <131979448+neerajthakur1@users.noreply.github.com>
2023-09-25 04:22:55 +0000

    test

---
+++ basics/test.js
@@ -0,0 +1,12 @@
+const accountId = 564654
+let accountEmail = "neeraj@google.com"
+var accountpassword ="123654789"
+
+console.log(accountEmail);
+
+// accountId = 867454
+accountEmail = "java@gmail.com"
+accountpassword = "cbnxkjb"
+accountCity = "delhi"
+
+console.table([accountId, accountEmail, accountpassword, accountCity])
